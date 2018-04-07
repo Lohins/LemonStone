e6e0392d34bcbff06e6e388c68997dcb6672403c
Sitong Chen <sitongchen@sitongs-macbook-pro.local>
2018-04-07 10:30:04 -0300

    Initial Commit

---
+++ LemonStone/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  LemonStone
+//
+//  Created by Sitong Chen on 2018-04-07.
+//  Copyright © 2018 Sitong Chen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ LemonStone/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LemonStone
+//
+//  Created by Sitong Chen on 2018-04-07.
+//  Copyright © 2018 Sitong Chen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
